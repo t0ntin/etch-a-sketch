082255902c0a4493295c89fd887b0ff96bf182b9
t0ntin <115113636+t0ntin@users.noreply.github.com>
2023-01-07 17:06:40 -0500

    Grid changes size, added reset button

--- js/script.js
+++ js/script.js
@@ -1,17 +1,54 @@
 const container = document.querySelector("#container");
-const numberOfSquares = prompt("enter a number between __ and __")
+const gridSizeBtn = document.querySelector("#grid-size-btn");
+const resetBtn = document.querySelector("#reset-btn");
 
+const generateSquares = () => {
+  let squareDiv = document.createElement("div");
+  squareDiv.classList.add("square");
+  container.append(squareDiv);
+};
 
-function createGrid() {
-  for (let i = 0; i < 256; i++) {
-    let squareDiv = document.createElement('div');
-    squareDiv.classList.add("square");
-    container.append(squareDiv);
+const adjustGrid = (numberOfSquares) => {
+  container.style.gridTemplateColumns = `repeat(${numberOfSquares}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${numberOfSquares}, 1fr)`;
+};
+
+const checkGridSize = (numberOfSquares) => {
+  let correctSize;
+  if (numberOfSquares > 3 && numberOfSquares < 81) {
+    correctSize = true;
+  } else {
+    correctSize = false;
+    alert("You didn't enter the correct number");
   }
+  return correctSize;
+};
+
+for (let i = 0; i < 256; i++) {
+  generateSquares();
 }
 
 container.addEventListener("mouseover", (event) => {
   event.target.classList.add("hovered");
-})
+});
 
-createGrid();
+const changeGridSize = (numberOfSquares) => {
+  let correctSize = checkGridSize(numberOfSquares);
+  if (correctSize) {
+    const totalSquares = numberOfSquares * numberOfSquares;
+    for (let i = 0; i < totalSquares; i++) {
+      generateSquares();
+    }
+  }
+  adjustGrid(numberOfSquares);
+};
+
+gridSizeBtn.onclick = () => {
+  const numberOfSquares = prompt("Enter a number between 4 and 80");
+  changeGridSize(numberOfSquares);
+};
+
+resetBtn.onclick = (numberOfSquares) => {
+  changeGridSize(16);
+  adjustGrid(numberOfSquares);
+};
